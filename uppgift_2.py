36ffb9912737d4d5ea67a17511da61dcf55cdccc
ayse <ayseko1989@gmail.com>
2026-05-24 19:29:24 +0200

    Uppgiften är förbättrad

--- uppgift_2.py
+++ uppgift_2.py
@@ -1,7 +1,7 @@
 längd = int(input("Ange din längd som cm : "))
 minsta_längd = 130
 
-if längd>=minsta_längd :
+if längd >= minsta_längd:
     print(
         "Du får åka!"
     )
--- uppgift_4.py
+++ uppgift_4.py
@@ -1,17 +1,22 @@
-
 fråga = input("Vill du ange temperatur i Celsius eller Fahrenheit? (C/F): ").upper()
 if fråga == "C":
-    input_celsius = float (input("Skriv in en temperatur i grader Celsius:"))
+    input_celsius = float(input("Skriv in en temperatur i grader Celsius:"))
     fahrenheit = (input_celsius * 1.8) + 32
     print("Det blir " + str(fahrenheit) + " grader Fahrenheit.")
+
+    if input_celsius < 10:
+        print("Ta på dig vinterkläder!")
+
+    elif input_celsius >= 20:
+        print("Packa badkläder :)")
+
 elif fråga == "F":
     input_fahrenheit = float(input("Skriv in en temperatur i grader Fahrenheit:"))
     celsius = (input_fahrenheit - 32) / 1.8
     print("Det blir " + str(celsius) + " grader Celsius.")
-    if celsius < 10 :
+    if celsius < 10:
         print("Ta på dig vinterkläder!")
-    elif celsius >= 20 :
-        print("packa badkläder :)")
+    elif celsius >= 20:
+        print("Packa badkläder :)")
 else:
     print("Ange \"F\" eller \"C\"")
-
